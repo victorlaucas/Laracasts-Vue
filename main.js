57e51ee03164c3c984e3fe32e33bd6ac176dab3c
Victor <victorlaucas@gmail.com>
2017-12-11 12:04:07 -0700

    Practical Component Exercise #2: Modal video. Created modal with click events so when button is clicked modal is shown and when x is clicked modal closes

--- main.js
+++ main.js
@@ -1,27 +1,21 @@
-Vue.component('message', {
-  props: ['title', 'body'],
-
-  data() {
-    return {
-      isVisible: true
-    };
-  },
-
+Vue.component('modal', {
   template: `
-    <article class="message" v-show="isVisible">
-      <div class="message-header">
-        {{ title }}
-
-        <button type="button" @click="isVisible = false">Hide</button>
+    <div class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        <div class="box">
+          <slot></slot>
+        </div>
       </div>
-      
-      <div class="message-body">
-        {{ body }}      
-      </div>
-    </article>
+      <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+    </div>
   `
 });
 
 new Vue({
-  el: '#root'
+  el: '#root',
+
+  data: {
+    showModal: false
+  }
 });
