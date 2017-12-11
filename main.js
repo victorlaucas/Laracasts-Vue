a759045164079adca06461fe18866579c876f26e
Victor <victorlaucas@gmail.com>
2017-12-11 10:53:56 -0700

    Components Within Components video

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-// New component. name followed by what you want it to do 
+Vue.component('task-list', {
+  template: `
+    <div>
+      <task v-for="task in tasks"> {{ task.description}}</task>
+    </div>
+    `, // Note that this is in back ticks ```
+
+  data(){
+    return {
+      tasks: [
+        {description: "Do something", completed: true},
+        {description: "Do something else", completed: false},
+        {description: "Watch videos", completed: true},
+        {description: "Read a book", completed: false},
+        {description: "Go code", completed: false},
+      ]
+    };
+  }
+});
+
 Vue.component('task', {
-  template: '<li><slot></slot></li>' // slot is used to get user input
+  template: '<li><slot></slot></li>' 
 });
 
 new Vue({
