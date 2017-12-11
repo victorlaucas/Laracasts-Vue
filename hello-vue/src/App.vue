8714905b29b6464d4db17a32e9c81f5b727148d1
Victor <victorlaucas@gmail.com>
2017-12-11 15:31:52 -0700

    Webpack and vue-cli video. Ran scaffold to build the basics of working with vue-cli

---
+++ hello-vue/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <message>Hello There</message>
+    <message>Hello There again</message>
+  </div>
+</template>
+
+<script>
+import Message from './components/Message.vue';
+
+export default {
+  name: 'app',
+
+  components: { Message },
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
